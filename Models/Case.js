2b44926061e1230e518a47d98e3c2e95c38cb783
AliElsebaie <Ali.elsebaie@student.guc.edu.eg>
2019-02-24 16:33:45 +0200

    'case'

--- Models/Case.js
+++ Models/Case.js
@@ -3,8 +3,9 @@ var Schema = mongoose.Schema;
  
 //mongoose.connect('mongodb://localhost/myappdatabase');
 
-var caseSchema = new Schema({
-      FormID : ObjectId,
+var Case = new Schema({
+      FormID : ObjectId,     //forgein key
+      CaseID : mongoose.Schema.Types.Number,
       CreationDate : {type:Date , required:true},
       LawyerSeen : Boolean ,
       LawyerApprove : Boolean ,
@@ -15,3 +16,6 @@ var caseSchema = new Schema({
       Currency :{type:String , required:true},
       EquityCapital :{type:String , required:true}
 });
+
+var Case = mongoose.model('Case',Case);
+module.exports = Case;

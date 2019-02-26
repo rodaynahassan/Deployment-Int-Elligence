9d3aa16b22fc64d2ea809e025b1d341f8fd57101
rodaynahassan <38787524+rodaynahassan@users.noreply.github.com>
2019-02-26 13:04:28 +0200

    Company

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 
 const investors = require('./routes/api/investors')
 const sscForms = require('./routes/api/sscforms')
-<<<<<<< HEAD
 const companies=require('./routes/api/companies')
-=======
 
 const lawyers = require('./routes/api/lawyers')
 
@@ -15,7 +13,6 @@ const admins = require('./routes/api/admins')
 
 const cases=require('./routes/api/cases')
 
->>>>>>> 5d73ff701722123cccd4bc4a648326515f57f2e4
 
 const app = express()
 app.use(express.json())
@@ -27,9 +24,7 @@ app.get('/', (req, res) => {
 // Direct routes to appropriate files 
 app.use('/routes/api/investors', investors)
 app.use('/routes/api/sscforms',sscForms)
-<<<<<<< HEAD
 app.use('/routes/api/companies',companies)
-=======
 
 app.use('/routes/api/lawyers',lawyers)
 
@@ -40,7 +35,6 @@ app.use('/routes/api/cases',cases)
 
 
 
->>>>>>> 5d73ff701722123cccd4bc4a648326515f57f2e4
 // Handling 404
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});

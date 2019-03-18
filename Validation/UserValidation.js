5b60d44ef392311c9a1198414828de054e45ac61
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-03-18 23:01:16 +0200

    validations of user

--- Validation/UserValidation.js
+++ Validation/UserValidation.js
@@ -3,8 +3,7 @@ const Joi = require('joi')
 module.exports = {
     createValidation: request => {
         const createSchema = {
-        firstName: Joi.string().required().max(50),
-        lastName:  Joi.string().required().max(50),
+        Name: Joi.string().required().max(50),
         gender:   Joi.string().required().max(6),
         nationality: Joi.string().required().max(50),
         identificationType:  Joi.string().required().max(50), 
@@ -22,8 +21,7 @@ module.exports = {
 
     updateValidation: request => {
         const updateSchema = {
-            firstName: Joi.string().max(50),
-            lastName:  Joi.string().max(50),
+            Name: Joi.string().max(50),
             gender:   Joi.string().max(6),
             nationality: Joi.string().max(50),
             identificationType:  Joi.string().max(50), 

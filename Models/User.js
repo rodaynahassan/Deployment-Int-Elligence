90e9342ebfab65da36eb88b75c85fa6e440f815d
AliElsebaie <ali_elsebaie98@yahoo.com>
2019-03-18 21:34:05 +0200

    merged investor, lawyer and reviewer into userschema

---
+++ Models/User.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Company = require('../../models/Company')
+
+
+var User = new Schema({
+
+  userType:{type:String,required: true},
+  name: {type:String,required:true},
+  gender: {type:String,required:true},
+  nationality: {type:String,required:true},
+  identificationType: {type:String,required:true},
+  identificationNumber: {type:String,required:true,unique:true},
+  birthdate: {type:date,required:true},
+  address: {type:String,required:true,unique:true},
+  telephone:{type:Number,unique:true},
+  fax: Number,
+  cases: {type: [String], required: true },
+  email: { type: String, unique: true} ,
+  password: { type: String, required: true },
+  forms: {type: [Object],required: true},
+  companies: {type: Company,required: true},
+  lawyer: {type: User,required: true},
+  investorType: {type: [String], required: true}
+  
+});
+
+
+
+module.exports = User = mongoose.model('users', User)

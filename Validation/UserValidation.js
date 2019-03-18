fd487af42707771e6fd765f032b20ace33491af7
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-03-18 23:08:22 +0200

    updated validations for user

--- Validation/UserValidation.js
+++ Validation/UserValidation.js
@@ -1,41 +1,159 @@
 const Joi = require('joi')
 
 module.exports = {
-    createValidation: request => {
+    createValidationL: request => {                         // create for lawyer
         const createSchema = {
-        Name: Joi.string().required().max(50),
+        userType: Joi.string().required(),    
+        name: Joi.string().required().max(50),
         gender:   Joi.string().required().max(6),
         nationality: Joi.string().required().max(50),
-        identificationType:  Joi.string().required().max(50), 
-        identificationNum:   Joi.string().required().max(50),
+        identificationType: Joi.string().required().max(50), 
+        identificationNum: Joi.string().required().max(50),
         birthDate:  Joi.date().required().max(50),
         address: Joi.string().required().max(50),
         telephone: Joi.string().max(20),
         fax:  Joi.string().max(20),
         email:  Joi.string().email().max(20),
-        password : Joi.string().required().min(8).max(50)
+        password : Joi.string().required().min(8).max(50),
+        cases: Joi.array.items(Joi.object()).required(),                //not sure yet
+        
+       
+
         }
+
 
         return Joi.validate(request, createSchema)
     },
 
-    updateValidation: request => {
+
+
+    createValidationI: request => {                                 // create for investor
+        const createSchema = {
+        userType: Joi.string().required(),    
+        name: Joi.string().required().max(50),
+        gender:   Joi.string().required().max(6),
+        nationality: Joi.string().required().max(50),
+        identificationType: Joi.string().required().max(50), 
+        identificationNum: Joi.string().required().max(50),
+        birthDate:  Joi.date().required().max(50),
+        address: Joi.string().required().max(50),
+        telephone: Joi.string().max(20),
+        fax:  Joi.string().max(20),
+        email:  Joi.string().email().max(20),
+        password : Joi.string().required().min(8).max(50),
+        forms: Joi.array.items(Joi.object()).required(),                //not sure yet
+        companies: Joi.array.items(Joi.object()).required(),            //not sure yet
+        lawyer: Joi.object().required(),                                //not sure yet
+        investorType: Joi.string().required()
+
+        }
+
+
+        return Joi.validate(request, createSchema)
+    },
+
+
+
+    createValidationR: request => {                             // create for reviewer
+        const createSchema = {
+        userType: Joi.string().required(),    
+        name: Joi.string().required().max(50),
+        gender:   Joi.string().required().max(6),
+        nationality: Joi.string().required().max(50),
+        identificationType: Joi.string().required().max(50), 
+        identificationNum: Joi.string().required().max(50),
+        birthDate:  Joi.date().required().max(50),
+        address: Joi.string().required().max(50),
+        telephone: Joi.string().max(20),
+        fax:  Joi.string().max(20),
+        email:  Joi.string().email().max(20),
+        password : Joi.string().required().min(8).max(50),
+        cases: Joi.array.items(Joi.object()).required(),                //not sure yet
+
+    
+        
+
+        }
+
+
+        return Joi.validate(request, createSchema)
+    },
+
+
+    updateValidationL: request => {                                     //update for lawyer
         const updateSchema = {
-            Name: Joi.string().max(50),
-            gender:   Joi.string().max(6),
-            nationality: Joi.string().max(50),
-            identificationType:  Joi.string().max(50), 
-            identificationNum:   Joi.string().max(50),
-            birthDate:  Joi.date().max(50),
-            address: Joi.string().max(50),
+            name: Joi.string().required().max(50),
+            gender:   Joi.string().required().max(6),
+            nationality: Joi.string().required().max(50),
+            identificationType: Joi.string().required().max(50), 
+            identificationNum: Joi.string().required().max(50),
+            birthDate:  Joi.date().required().max(50),
+            address: Joi.string().required().max(50),
             telephone: Joi.string().max(20),
             fax:  Joi.string().max(20),
             email:  Joi.string().email().max(20),
-            password : Joi.string().min(8).max(50)
+            password : Joi.string().required().min(8).max(50),
+            cases: Joi.array.items(Joi.object()).required(),                //not sure yet
         }
 
         return Joi.validate(request, updateSchema)
     }, 
+
+
+
+
+    updateValidationI: request => {                                     //update for investor
+        const updateSchema = {
+            name: Joi.string().required().max(50),
+            gender:   Joi.string().required().max(6),
+            nationality: Joi.string().required().max(50),
+            identificationType: Joi.string().required().max(50), 
+            identificationNum: Joi.string().required().max(50),
+            birthDate:  Joi.date().required().max(50),
+            address: Joi.string().required().max(50),
+            telephone: Joi.string().max(20),
+            fax:  Joi.string().max(20),
+            email:  Joi.string().email().max(20),
+            password : Joi.string().required().min(8).max(50),
+            forms: Joi.array.items(Joi.object()).required(),                //not sure yet
+            companies: Joi.array.items(Joi.object()).required(),            //not sure yet
+            lawyer: Joi.object().required(),                                //not sure yet
+            investorType: Joi.string().required()
+    
+        }
+
+        return Joi.validate(request, updateSchema)
+    }, 
+
+
+
+
+
+    updateValidationR: request => {                                             //update for reviewer
+        const updateSchema = {
+            name: Joi.string().required().max(50),
+        gender:   Joi.string().required().max(6),
+        nationality: Joi.string().required().max(50),
+        identificationType: Joi.string().required().max(50), 
+        identificationNum: Joi.string().required().max(50),
+        birthDate:  Joi.date().required().max(50),
+        address: Joi.string().required().max(50),
+        telephone: Joi.string().max(20),
+        fax:  Joi.string().max(20),
+        email:  Joi.string().email().max(20),
+        password : Joi.string().required().min(8).max(50),
+        forms: Joi.array.items(Joi.object()).required(),                //not sure yet
+        companies: Joi.array.items(Joi.object()).required(),            //not sure yet
+        lawyer: Joi.object().required(),                                //not sure yet
+        investorType: Joi.string().required()
+
+        }
+
+        return Joi.validate(request, updateSchema)
+    }, 
+
+
+
 }
 
 

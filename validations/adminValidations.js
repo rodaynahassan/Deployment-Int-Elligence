cc88e8e9049abfd25a098076aac88e955d44f4e8
abdelrahmanaamin <abdelrahman.amin@student.guc.edu.eg>
2019-03-19 18:09:02 +0200

    validations for admin

--- validations/adminValidations.js
+++ validations/adminValidations.js
@@ -3,17 +3,17 @@ const Joi = require('joi')
 module.exports = {
     createValidation: request => {
         const createSchema = {
-            name: Joi.string().required(),
-            gender: Joi.string().required(),
-            nationality: Joi.string().required(),
-            identificationType: Joi.string().required(),
+            name: Joi.string().required().max(50),
+            gender: Joi.string().required().max(6),
+            nationality: Joi.string().required().max(50),
+            identificationType: Joi.string().required().max(50),
             identificationNumber: Joi.string().max(14).required(),
-            password: Joi.string().min(8).required(),
-            birthdate: Joi.date().required(),
-            address: Joi.string().required(),
-            telephone: Joi.string().min(8).max(15),
-            fax: Joi.string(),
-            email: Joi.string().email()
+            password: Joi.string().min(8).required().max(50),
+            birthdate: Joi.date().required().max(50),
+            address: Joi.string().required().max(50),
+            telephone: Joi.string().min(8).max(20),
+            fax: Joi.string().max(20),
+            email: Joi.string().email().max(20)
         }
 
         return Joi.validate(request, createSchema)
@@ -21,17 +21,17 @@ module.exports = {
 
     updateValidation: request => {
         const updateSchema = {
-            name: Joi.string(),
-            gender: Joi.string(),
-            nationality: Joi.string(),
-            identificationType: Joi.string(),
+            name: Joi.string().max(50),
+            gender: Joi.string().max(6),
+            nationality: Joi.string().max(50),
+            identificationType: Joi.string().max(50),
             identificationNumber: Joi.string().max(14),
-            password: Joi.string().min(8),
-            birthdate: Joi.date(),
-            address: Joi.string(),
-            telephone: Joi.string().min(8).max(15),
-            fax: Joi.string(),
-            email: Joi.string().email()
+            password: Joi.string().min(8).max(50),
+            birthdate: Joi.date().max(50),
+            address: Joi.string().max(50),
+            telephone: Joi.string().min(8).max(20),
+            fax: Joi.string().max(20),
+            email: Joi.string().email().max(20)
         }
 
         return Joi.validate(request, updateSchema)

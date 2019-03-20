85d4782557becf55a26beb423103da0e9e5fc368
omarkoraa <omar.koraa@student.guc.edu.eg>
2019-03-20 16:22:12 +0200

    Resolved Form and form conflict

--- Models/Admin.js
+++ Models/Admin.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Case = require('../../Models/Case');
 
 const Admin = new Schema({
   name: { type: String, required: true},

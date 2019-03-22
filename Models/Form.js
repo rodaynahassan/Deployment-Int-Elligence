fd3a26375f76d85efda2a4ee7102fd6d2273f2ff
NadineMoustafa <47187897+NadineMoustafa@users.noreply.github.com>
2019-03-23 00:17:51 +0200

    Testing

--- Models/Form.js
+++ Models/Form.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-
 const SSCManager= new Schema({
     name: {
         type: String,
@@ -18,7 +17,6 @@ const SSCManager= new Schema({
         type:String,
         required: true
     },
-
      identificationType: {
         type: String,
         required: true

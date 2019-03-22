1e16948d09fc46f32c4614ae300316599dac3eab
omarkoraa <omar.koraa@student.guc.edu.eg>
2019-03-22 17:57:37 +0200

    Mongo DB Test

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 const User = require('../../Models/User')
-const validator = require('../../Validation/UserValidations')
+const validator = require('../../Validation/UserValidation')
 
 // view a certain user
 router.get('/:id', async(req, res) => {

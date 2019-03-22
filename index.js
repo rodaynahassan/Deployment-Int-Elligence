8a5550d2ccdaeed2df9030fa58b1e6a6d886182c
omarkoraa <omar.koraa@student.guc.edu.eg>
2019-03-22 19:21:02 +0200

    Heroku and Mongo Done with Admin tests

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const forms = require('./routes/api/forms')
 const admins = require('./routes/api/admins')
 const cases=require('./routes/api/cases')
 
+const app = express()
 // DB Config
 const db = require('./config/keys').mongoURI
 
 // Connect to mongo
 mongoose
-    .connect(db)
+    .connect('mongodb+srv://ScrumMaster:26312215@int-elligence-s1doh.mongodb.net/local_library?retryWrites=true')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err))
 
-const app = express()
 // Init middleware
 
-app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`);

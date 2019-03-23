1df916bedbd1069f53b2c55b30df39dad56f7274
omarkoraa <omar.koraa@student.guc.edu.eg>
2019-03-23 19:32:57 +0200

    Testing Phase Pt 6

--- routes/api/forms.js
+++ routes/api/forms.js
@@ -53,18 +53,17 @@ router.post('/', async (req,res) => {
     //update a company
  router.put('/:id', async (req,res) => {
     try {
-        if(req.body.type==='SSC'){
+        
      const id = req.params.id
-     const ssc = await Form.findById(id)
+     const form = await Form.findById(id)
+     if(form.type==='SSC'){
      if(!ssc) return res.status(404).send({error: 'SSC Form does not exist'})
      const isValidated = validator. updateValidationSSC(req.body)
      if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
      const updatedSSC = await Form.findByIdAndUpdate(id,req.body)
      res.json({msg: 'SSCForm updated successfully'})
         }
-        if(req.body.type==='SPC'){
-            const id = req.params.id
-            const spc = await Form.findById(id)
+        if(form.type==='SPC'){
             if(!spc) return res.status(404).send({error: 'SPC Form does not exist'})
             const isValidated = validator. updateValidationSPC(req.body)
             if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })

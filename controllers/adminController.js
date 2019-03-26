bcc4b75d590b9407ade1cb628a1f3637305ede84
rodaynahassan <38787524+rodaynahassan@users.noreply.github.com>
2019-03-26 20:31:35 +0200

    controllers

---
+++ controllers/adminController.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Admin = require('../../Models/Admin');
+const adminValidator = require('../../Validation/adminvalidations')
+const Case = require('../../Models/Case');
+
+exports.create=async function search (att,value)
+{
+    if (!att)
+    {
+        var admins=await Admin.find()
+        return admins
+    }
+    if (att==='id')
+    {
+        var certainAdmin=await Admin.findById(value)
+        return certainAdmin
+    }
+}
+exports.create=async function create(body)
+{
+    try
+    {
+      var isAdminValidated=adminValidator.createValidation(req.body)
+      if (isAdminValidated.error) return {error:isAdminValidated.error.details[0].message}
+      const newAdmin=await Admin.create(req.body)
+      return newAdmin
+
+
+    }
+    catch (error) 
+    {
+        console.log(error)
+    }
+}
--- routes/api/admins.js
+++ routes/api/admins.js
@@ -3,18 +3,32 @@ const express = require('express');
 const uuid = require('uuid');
 const router = express.Router();
 const validator = require('../../Validation/adminValidations')
-
+const controller = require('../../Controllers/adminController')
 
 // Models
 const Admin = require('../../Models/Admin');
 const Cases = require('../../Models/Case');
 
-
-// Get admins
-router.get('/', async (req,res) => {
-	const admins = await Admin.find()
-	res.json({data: admins})
+//controller
+router.get('/getById/:id',async(req,res) => 
+{
+    var admin=adminController.search("id",req.params.id)
+    return res.json({ data: cases });
 })
+
+
+router.get('/',async(req,res) => 
+{
+    const admin=adminController.search()
+    return res.json({ data: cases });
+})
+router.post('/', async (req,res) => {
+    const newAdmin=adminController.create(req.body)
+    return res.json({ data: newAdmin });
+ })
+
+// 
+
 router.get('/CasesSortedById', async(req, res) => {
     var cases= await Cases.find()
     cases.sort(compareById)
@@ -26,16 +40,8 @@ router.get('/CasesSortedByCreationDate', async(req, res) => {
     cases.sort(compare)
     return res.json({ data: cases });
 })
-router.get('/:id', async(req, res) => {
-    const id=req.params.id
-    const admins= await Admin.findById(id)
-    return res.json({ data: admins});
-})
 
 //sort cases by ID
-
-
-
 function compareById(a,b){
     if(a._id < b._id) return -1
     if(b._id < a._id) return 1
@@ -43,63 +49,11 @@ function compareById(a,b){
     return 0
 }
 
-
-
-
 //View the sorted cases by date
-
 function compare(a,b){
     if(Date.parse(a.creationDate)>Date.parse(b.creationDate)) return 1
     if(Date.parse(a.creationDate)<Date.parse(b.creationDate)) return -1
     return 0
 }
-
-
-
-
-// Create a new admin
-
-router.post('/', async (req,res) => {
-   try {
-    const isValidated = validator.createValidation(req.body)
-    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-    const newAdmin = await Admin(req.body).save()
-    res.json({msg:'Admin was created successfully', data: newAdmin})
-   }
-   catch(error) {
-       // We will be handling the error later
-       console.log(error)
-   }  
-})
-
-router.put('/:id', async (req,res) => {
-    try {
-     const id = req.params.id
-     const admin = await Admin.findById(id)
-     if(!admin) return res.status(404).send({error: 'Admin does not exist'})
-     const isValidated = validator.updateValidation(req.body)
-     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-     const x = await Admin.findByIdAndUpdate(id,req.body)
-     const updatedAdmin = Admin.findById(id)
-     res.json({msg: 'Admin updated successfully',data:updatedAdmin})
-    }
-    catch(error) {
-        // We will be handling the error later
-        console.log(error)
-    }  
- })
-
-router.delete('/:id', async (req, res) => {
-	try{
-    const adminId = req.params.id 
-    const deletedAdmin = await Admin.findByIdAndRemove(adminId)
-	res.json({msg:'Admin was deleted successfully', data: deletedAdmin})
-	}
-	catch(error){
-		console.log(error)
-	}
-})
-
-
 
 module.exports = router;

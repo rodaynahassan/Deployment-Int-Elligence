e465f8cd19d5120f6a6a38fa89268c59360d77b8
abdelrahmanaamin <abdelrahman.amin@student.guc.edu.eg>
2019-03-26 21:54:55 +0200

    Controller updated

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -65,3 +65,20 @@ exports.remove = async function remove(att,value){
         return admins
     }
 }
+
+
+//sort cases by ID
+function compareById(a,b){
+    if(a._id < b._id) return -1
+    if(b._id < a._id) return 1
+    
+    return 0
+}
+
+//View the sorted cases by date
+function compare(a,b){
+    if(Date.parse(a.creationDate)>Date.parse(b.creationDate)) return 1
+    if(Date.parse(a.creationDate)<Date.parse(b.creationDate)) return -1
+    return 0
+}
+

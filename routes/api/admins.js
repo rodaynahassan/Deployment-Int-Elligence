8b29c0e21fa982c1ed577d7e47e55ea7ac161cdf
abdelrahmanaamin <abdelrahman.amin@student.guc.edu.eg>
2019-03-26 21:47:27 +0200

    Controller

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -41,21 +41,6 @@ router.get('/CasesSortedByCreationDate', async(req, res) => {
     return res.json({ data: cases });
 })
 
-//sort cases by ID
-function compareById(a,b){
-    if(a._id < b._id) return -1
-    if(b._id < a._id) return 1
-    
-    return 0
-}
-
-//View the sorted cases by date
-function compare(a,b){
-    if(Date.parse(a.creationDate)>Date.parse(b.creationDate)) return 1
-    if(Date.parse(a.creationDate)<Date.parse(b.creationDate)) return -1
-    return 0
-}
-
 
 
 

bba6aa070e45e42b449a38bc7c9aa49fbb146bd6
rodaynahassan <38787524+rodaynahassan@users.noreply.github.com>
2019-03-29 22:33:32 +0200

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -59,7 +59,7 @@ router.post('/register', async (req,res) => {                       //register I
     if(newUser.error) return res.status(400).send(newUser) 
      return res.json({msg:'Investor was created successfully', data: newUser})
  })
- 
+
 //Login
 router.post('/login',async(req,res)=>{
 {
@@ -84,7 +84,7 @@ router.post('/login',async(req,res)=>{
         return res.status(400).send({ password: 'Wrong password' });   
 }
 catch(e){}
-}
+})
 
 
 //update a user
@@ -108,11 +108,3 @@ router.get('/getCases/:id',async(req,res) => {
 });
 
 module.exports = router;
-
-
-
-
-
-
-
-

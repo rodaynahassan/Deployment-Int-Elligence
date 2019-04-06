8f938f7c7f5547ae7bb07b1aaedd6f3decc13d8e
NadaLabib <nada.elazzab@student.guc.edu.eg>
2019-04-06 21:41:51 +0200

    choose the type

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const users = require('./routes/api/users')
 const forms = require('./routes/api/forms')
 const admins = require('./routes/api/admins')
 const externalentities=require('./routes/api/externalentities')
+const cors = require('cors');
 const app = express()
+
 // DB Config
 const db = require('./config/keys').mongoURI
+
 
 // Connect to mongo
 mongoose
@@ -20,6 +23,7 @@ mongoose
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors())
 //app.use(express.multipart());
 
 app.get('/', (req, res) => {
@@ -32,6 +36,7 @@ app.use('/routes/api/forms',forms)
 app.use('/routes/api/admins',admins)
 app.use('/routes/api/externalentities',externalentities)
 
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -41,7 +46,7 @@ app.use(function(req, res, next) {
 
 // Handling 404
 app.use((req, res) => {
-    res.status(404).send({err: 'We can not find what you are looking for'});
+     res.status(404).send({err: 'We can not find what you are looking for'});
  })
 
 const port =   process.env.PORT ||  5000  

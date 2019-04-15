a100125a3a2271c09e59884f5316d4fd13fde71e
NadaLabib <nada.elazzab@student.guc.edu.eg>
2019-04-15 17:13:33 +0200

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors =require('cors')
+const path = require ('path')
 const users = require('./routes/api/users')
 const forms = require('./routes/api/forms')
 const admins = require('./routes/api/admins')
@@ -22,13 +23,13 @@ mongoose
     { useNewUrlParser: true }
 
 
-    if(process.env.NODE_ENV==='production'){
-        app.use(express.static('client/build'));
-        app.get('*',(req,res)=> {
-    
-            res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-        });
-    }
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=> {
+
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 
 // Init middleware

7b96941b5f0589e0c6f2ef1dd6ba3263f2c70e1b
NadaLabib <nada.elazzab@student.guc.edu.eg>
2019-04-15 16:42:24 +0200

    deploy

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ app.use(express.urlencoded({extended: false}))
 //app.use(express.multipart());
 app.use(cors())
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=> {
+
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+
+
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`);
 })

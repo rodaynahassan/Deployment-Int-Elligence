4fc55a5bc5ada8c451f133648565a3dbcacaaad8
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-04-22 01:12:09 +0200

    deploy

--- server.js
+++ server.js
@@ -27,29 +27,29 @@ mongoose
 // 	res.sendFile(path.resolve(where, the, index.html, is));
 // });
 
-//Static file declaratio
-// app.use(express.static(path.join(__dirname, 'client/build')));
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// //production mode
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static(path.join(__dirname, 'client/build')));
-// 	//
-// 	app.get('*', (req, res) => {
-// 		res.sendfile(path.join(((__dirname = 'client'), 'build', 'index.html')));
-// 	});
-// }
-
-// //build mode
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.join(__dirname + '/client/public/index.html'));
-// });
-
+//production mode
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	//
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendfile(path.join(((__dirname = 'client'), 'build', 'index.html')));
 	});
 }
+
+//build mode
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
+
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
 
 // Init middleware
 

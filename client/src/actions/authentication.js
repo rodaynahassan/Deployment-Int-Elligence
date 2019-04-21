d1725a7169f8241057b2c76ec9d5c602800ec7f5
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-04-21 22:09:54 +0200

    deployment

--- client/src/actions/authentication.js
+++ client/src/actions/authentication.js
@@ -41,7 +41,7 @@ export const registerLR = (user) => (dispatch) => {
 
 export const loginUser = (user) => (dispatch) => {
 	axios
-		.post('http://localhost:5000/routes/api/users/login', user)
+		.post('http://desolate-oasis-18053.herokuapp.com/login', user)
 		.then((res) => {
 			const { token } = res.data;
 			localStorage.setItem('jwtToken', token);

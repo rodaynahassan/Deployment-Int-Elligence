f523a9f2d3122266a269d8ba244687b58e8eab96
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-04-22 02:38:19 +0200

    deploy

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ mongoose
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
+console.log(process.env.NODE_ENV + '   ffffffffffffffffffffff');
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	//
@@ -109,6 +110,6 @@ app.use((req, res) => {
 	res.status(404).send({ err: 'We can not find what you are looking for' });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`));

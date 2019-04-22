aebb2336af19bc1cb1b4fc986a809388b73f5d7c
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-04-22 12:37:59 +0200

    deploy

--- client/src/components/form/DropdownNationalities.js
+++ client/src/components/form/DropdownNationalities.js
@@ -1,47 +1,45 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
-import {Dropdown} from 'react-bootstrap';
-import {Col} from 'react-bootstrap';
-import {Row} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import { DropdownMenu, MenuItem } from 'react-bootstrap-dropdown-menu';
-import { Redirect } from 'react-router-dom'
-import Footer from '../layout/footer'
+import { Redirect } from 'react-router-dom';
+import Footer from '../layout/footer';
 import axios from 'axios';
 
 class DropdownTrial extends Component {
+	state = {
+		nationalities: []
+	};
+	tabRow() {
+		return this.state.nationalities.map(function(nationality, i) {
+			return (
+				<Dropdown.Item key={i}>
+					<h6>{nationality.name}</h6>
+				</Dropdown.Item>
+			);
+		});
+	}
+	componentDidMount() {
+		axios.get('http://localhost:' + process.env.PORT + '/routes/api/nationalities/').then((res) => {
+			this.setState({ nationalities: res.data.data });
+		});
+	}
 
-    state = {
-        nationalities:[]
-      }
-    tabRow(){
-        return this.state.nationalities.map(function(nationality,i){
-            return <Dropdown.Item key={i} ><h6>{nationality.name}</h6></Dropdown.Item>;
-        });
-    }
-    componentDidMount(){
-        axios.get('http://localhost:5000/routes/api/nationalities/')
-        .then(res => {
-            this.setState({nationalities: res.data.data})
-        })}
-
-    render()
-    {
-        return (
-            <div>
-                <Dropdown>
-                <Dropdown.Toggle variant="dark" id="dropdown-basic" align="middle" >
-                 Choose Your Nationality
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                { this.tabRow() }
-                </Dropdown.Menu>
-                  </Dropdown>
-            </div>
-        )
-
-
-    }
+	render() {
+		return (
+			<div>
+				<Dropdown>
+					<Dropdown.Toggle variant="dark" id="dropdown-basic" align="middle">
+						Choose Your Nationality
+					</Dropdown.Toggle>
+					<Dropdown.Menu>{this.tabRow()}</Dropdown.Menu>
+				</Dropdown>
+			</div>
+		);
+	}
 }
 
-    export default DropdownTrial
+export default DropdownTrial;

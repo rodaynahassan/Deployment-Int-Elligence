e661f3fe1f6cedb14fddde44092bd357c40b7a58
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-04-22 03:49:06 +0200

    deploy

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ mongoose
 //roduction mode
 // console.log(process.env.NODE_ENV + '   ffffffffffffffffffffff');
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static(path.resolve(__dirname, 'client/build')));
 	//
 	app.get('*', (req, res) => {
-		res.sendfile(path.join((__dirname = 'client/build/index.html')));
+		res.sendfile(path.resolve((__dirname = 'client/build/index.html')));
 	});
 }
 // console.log(__dirname + '   gggggggggggggggggggg');

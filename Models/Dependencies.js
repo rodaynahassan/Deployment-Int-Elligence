62acfa353caf77ba53fdf707f371ac237e8c4bb4
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-04-23 07:18:13 +0200

    deploy

--- Models/Dependencies.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const Dependencies = new Schema({}, { strict: false });
-
-module.exports = dependencies = mongoose.model("dependencies", Dependencies);
--- Models/FormType.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const FormType = new Schema({},{ strict: false });
-
-module.exports = formType = mongoose.model('formtypes', FormType);

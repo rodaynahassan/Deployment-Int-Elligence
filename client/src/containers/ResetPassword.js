70d7ba033962755f5208325ff20411fca7501497
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-04-29 16:30:00 +0200

    deploy

--- client/src/containers/ResetPassword.js
+++ client/src/containers/ResetPassword.js
@@ -4,7 +4,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { MDBInput } from 'mdbreact';
+import { blue200 } from 'material-ui/styles/colors';
 
 import {
 	LinkButtons,
@@ -35,7 +38,8 @@ export default class ResetPassword extends Component {
 			password: '',
 			updated: false,
 			isLoading: true,
-			error: false
+			error: false,
+			showNullError: false
 		};
 	}
 
@@ -57,7 +61,7 @@ export default class ResetPassword extends Component {
 				}
 			})
 			.catch((error) => {
-				alert(error.response.data.errmsg || error.response.data)
+				alert(error.response.data.errmsg || error.response.data);
 				this.setState({
 					updated: false,
 					isLoading: false,
@@ -75,40 +79,76 @@ export default class ResetPassword extends Component {
 	updatePassword = (e) => {
 		console.log(this.state.name);
 		e.preventDefault();
-		axios
-			.put('/routes/api/users/updatePasswordViaEmail', {
-				name: this.state.name,
-				password: this.state.password,
-				resetPasswordToken: this.props.match.params.token
-			})
-			.then((response) => {
-				console.log(response.data);
-				if (response.data.message === 'password updated') {
-					this.setState({
-						updated: true,
-						error: false
-					});
-				} else {
-					this.setState({
-						updated: false,
-						error: true
-					});
-				}
-			})
-			.catch((err) => alert(err.response.data.errmsg || err.response.data));
+		if (this.state.password === '') {
+			this.setState({
+				showNullError: true
+			});
+		} else {
+			this.setState({
+				showNullError: false
+			});
+			axios
+				.put('/routes/api/users/updatePasswordViaEmail', {
+					name: this.state.name,
+					password: this.state.password,
+					resetPasswordToken: this.props.match.params.token
+				})
+				.then((response) => {
+					console.log(response.data);
+					if (response.data.message === 'password updated') {
+						this.setState({
+							updated: true,
+							error: false
+						});
+					} else {
+						this.setState({
+							updated: false,
+							error: true
+						});
+					}
+				})
+				.catch((err) => alert(err.response.data.errmsg || err.response.data));
+		}
 	};
-
 	render() {
-		const { password, error, isLoading, updated } = this.state;
+		const { password, error, isLoading, updated, showNullError } = this.state;
 
 		if (error) {
 			return (
 				<div>
-					<HeaderBar title={title} />
-					<div style={loading}>
-						<h4>Problem resetting password. Please send another reset link.</h4>
-						<LinkButtons buttonText="Go Home" buttonStyle={homeButton} link="/" />
-						<LinkButtons buttonStyle={forgotButton} buttonText="Forgot Password?" link="/forgotPassword" />
+					<div
+						style={{
+							backgroundColor: '#a3dbf1',
+							paddingTop: '10px',
+							textAlign: 'center',
+							fontSize: '50px',
+							color: 'dark',
+							flexDirection: 'row',
+							justifyContent: 'flex-end',
+							height: '140px'
+						}}
+					>
+						Reset Password
+						<br />
+						<div style={loading}>
+							<h4>Problem resetting password. Please send another reset link.</h4>
+							<Button
+								// buttonStyle={forgotButton}
+								className="btn-block btn-rounded z-depth-1a"
+								variant="omar"
+								href="/forgotPassword"
+								style={{
+									marginTop: '50px',
+									marginLeft: '50px',
+									width: '180px',
+									height: '40px',
+									backgroundColor: '#a3dbf1'
+								}}
+							>
+								Forgot Password?
+							</Button>
+							{/* <LinkButtons buttonStyle={forgotButton} buttonText="Forgot Password?" link="/forgotPassword" /> */}
+						</div>
 					</div>
 				</div>
 			);
@@ -116,33 +156,110 @@ export default class ResetPassword extends Component {
 		if (isLoading) {
 			return (
 				<div>
-					<HeaderBar title={title} />
-					<div style={loading}>Loading User Data...</div>
+					<div
+						style={{
+							backgroundColor: '#a3dbf1',
+							paddingTop: '70px',
+							textAlign: 'center',
+							fontSize: '50px',
+							color: 'dark',
+							flexDirection: 'row',
+							justifyContent: 'flex-end',
+							height: '155px'
+						}}
+					>
+						Reset Password
+						<br />
+						<div style={loading}>Loading User Data...</div>
+					</div>
 				</div>
 			);
 		}
 		return (
 			<div>
-				<HeaderBar title={title} />
-				<form className="password-form" onSubmit={this.updatePassword}>
-					<TextField
-						style={inputStyle}
-						id="password"
-						label="password"
-						onChange={this.handleChange('password')}
-						value={password}
-						type="password"
-					/>
-					<SubmitButtons buttonStyle={updateButton} buttonText="Update Password" />
-				</form>
-
-				{updated && (
-					<div>
-						<p>Your password has been successfully reset, please try logging in again.</p>
-						<LinkButtons buttonStyle={loginButton} buttonText="Login" href="/login" />
-					</div>
-				)}
-				<LinkButtons buttonText="Go Home" buttonStyle={homeButton} link="/" />
+				<div
+					style={{
+						backgroundColor: '#a3dbf1',
+						paddingTop: '8px',
+						textAlign: 'center',
+						fontSize: '50px',
+						color: 'dark',
+						flexDirection: 'row',
+						justifyContent: 'flex-end',
+						height: '80px'
+					}}
+				>
+					Reset Password
+					<br />
+					<form className="password-form" onClick={this.updatePassword}>
+						<MDBInput
+							style={{ width: '500px', right: '100%' }}
+							id="password"
+							label="Password"
+							value={password}
+							type="password"
+							onChange={this.handleChange('password')}
+						/>
+						<Button
+							buttonStyle={forgotButton}
+							className="btn-block btn-rounded z-depth-1a"
+							variant="omar"
+							style={{
+								marginTop: '50px',
+								marginLeft: '50px',
+								marginRight: '2500px',
+								width: '180px',
+								height: '40px',
+								backgroundColor: '#a3dbf1'
+							}}
+						>
+							Update password
+						</Button>
+						{/* <SubmitButtons buttonStyle={updateButton} buttonText="Update Password" /> */}
+					</form>
+					{showNullError && (
+						<div style={{ marginRight: '1170px', color: blue200 }}>
+							<h4>You must insert a password</h4>
+						</div>
+					)}
+					{updated && (
+						<div style={{ marginRight: '1170px', color: blue200 }}>
+							<h4>Your password has been successfully reset, please try logging in again.</h4>
+							<Button
+								buttonStyle={loginButton}
+								className="btn-block btn-rounded z-depth-1a"
+								variant="omar"
+								href="/login"
+								style={{
+									marginTop: '50px',
+									marginLeft: '50px',
+									marginRight: '2500px',
+									width: '100px',
+									height: '40px',
+									backgroundColor: '#a3dbf1'
+								}}
+							>
+								Login
+							</Button>
+						</div>
+					)}
+					<Button
+						buttonStyle={homeButton}
+						className="btn-block btn-rounded z-depth-1a"
+						variant="omar"
+						href="/"
+						style={{
+							marginTop: '50px',
+							marginLeft: '50px',
+							marginRight: '2500px',
+							width: '100px',
+							height: '40px',
+							backgroundColor: '#a3dbf1'
+						}}
+					>
+						Home
+					</Button>
+				</div>
 			</div>
 		);
 	}
